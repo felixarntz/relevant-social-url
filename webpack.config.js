484d43fe50dc0fe7cb500386e818aaf69f03d133
Felix Arntz <flixos90@gmail.com>
2025-01-27 16:58:46 -0800

    Fix broken JS build process.

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,6 @@
  */
 const config = require( '@wordpress/scripts/config/webpack.config' );
 
-const { sync: glob } = require( 'fast-glob' );
-const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils' );
-
-/**
- * Gets the entry points for the webpack configuration.
- *
- * This function extends the original entry points from the relevant '@wordpress/scripts' function with any index files
- * in one level deep directories in src.
- *
- * @return {Function} A function returning the entry points object.
- */
-function getEntryPoints() {
-	const entryPoints = getWebpackEntryPoints();
-
-	const [ entryFile ] = glob(
-		`${ process.env.WP_SRC_DIRECTORY }/index.[jt]s?(x)`,
-		{
-			absolute: true,
-		}
-	);
-	if ( entryFile ) {
-		entryPoints.index = entryFile;
-	}
-
-	return entryPoints;
-}
-
 module.exports = {
 	...config,
-	entry: getEntryPoints,
 };
