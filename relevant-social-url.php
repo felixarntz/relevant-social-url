a9d6bd0cfeb21d679016dffa9ff088ae0a74f58b
Felix Arntz <flixos90@gmail.com>
2026-08-09 15:10:40 -0500

    bump minimum required WordPress version to 6.1

--- relevant-social-url.php
+++ relevant-social-url.php
@@ -10,7 +10,7 @@
  * Plugin URI: https://wordpress.org/plugins/relevant-social-url/
  * Description: Allows to associate a social media post URL with each post, and optionally to display a link to it in the frontend.
  * Version: 1.0.1
- * Requires at least: 6.0
+ * Requires at least: 6.1
  * Requires PHP: 7.4
  * Author: Felix Arntz
  * Author URI: https://felix-arntz.me
